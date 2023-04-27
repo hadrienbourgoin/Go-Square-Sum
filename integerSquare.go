25ca8e49171f5f52cf9ea1bd2d333725f4357808
Hadri Bourgoin <kingpong@live.fr>
2023-04-27 16:09:21 +0200

    change 3

--- integerSquare.go
+++ integerSquare.go
@@ -7,10 +7,14 @@ import (
 
 func main() {
 	var s string
-	fmt.Println("Choose the number of test cases, it has to be an integer: ")
+	fmt.Println("Choose the number of test cases, it has to be an integer 1<=i<=100: ")
 	fmt.Scanln(&s)
 	if b, i := controluserinput(s); b {
-		looptestcase(i)
+		if i >= 1 && i <= 100 {
+			looptestcase(i)
+		} else {
+			fmt.Println("It has to be between 1 and 100.")
+		}
 	} else {
 		fmt.Println("It wasn't an integer.")
 	}
@@ -27,12 +31,16 @@ func controluserinput(uinput string) (bool, int) {
 func looptestcase(n int) {
 	var s string
 	if n > 0 {
-		fmt.Println("Choose the number of integers you want: ")
+		fmt.Println("Choose the number of integers you want 1<=i<=100: ")
 		fmt.Scanln(&s)
 		if b, i := controluserinput(s); b {
-			n--
-			fmt.Println("You'll have to enter ", i, " integers: ")
-			loopsquare(i, 0)
+			if i >= 1 && i <= 100 {
+				n--
+				fmt.Println("You'll have to enter ", i, " integers: ")
+				loopsquare(i, 0)
+			} else {
+				fmt.Println("It has to be between 1 and 100.")
+			}
 		} else {
 			fmt.Println("It wasn't an integer.")
 		}
@@ -43,16 +51,22 @@ func looptestcase(n int) {
 func loopsquare(n int, sum int) {
 	var s string
 	if n > 0 {
-		fmt.Println("Enter an integer: ")
+		fmt.Println("Enter an integer -100<=i<=100: ")
 		fmt.Scanln(&s)
 		if b, i := controluserinput(s); b {
-			n--
-			sum = sum + i*i
+			if i >= -100 && i <= 100 {
+				n--
+				if i > 0 {
+					sum = sum + i*i
+				}
+			} else {
+				fmt.Println("It has to be between -100 and 100.")
+			}
 		} else {
 			fmt.Println("It wasn't an integer.")
 		}
 		loopsquare(n, sum)
 	} else {
-		fmt.Println("The sum of square root of all your integers is: ", sum)
+		fmt.Println("The sum of square root of all your integers excluding negatives is: ", sum)
 	}
 }
